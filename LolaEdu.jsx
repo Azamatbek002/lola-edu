b473fb2b22630567df936a4b18178f78d44b2b7c
Azamatbek002 <azamatbekrahimov961@gmail.com>
2026-06-05 17:09:56 +0500

    LolaEdu.jsx

--- LolaEdu.jsx
+++ LolaEdu.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect, useRef } from "react";
-
 const STYLE = `
   @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600;700&family=DM+Sans:wght@300;400;500;600&display=swap');
   :root {
